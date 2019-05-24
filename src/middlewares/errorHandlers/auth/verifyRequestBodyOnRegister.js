72d15ea06d0d9ceddeecfd02ec3fd39d33a34d9b
Yilun Weng <wengellen@gmail.com>
2019-05-23 23:41:18 -0700

    Reset registratino verification for staff

--- src/middlewares/errorHandlers/auth/verifyRequestBodyOnRegister.js
+++ src/middlewares/errorHandlers/auth/verifyRequestBodyOnRegister.js
@@ -1,9 +1,9 @@
 module.exports = (request, response, next) => {
   if (request.body.username &&
       request.body.password &&
-      request.body.email &&
-      request.body.birthday &&
-      request.body.ss_id) {
+      request.body.email){
+      // request.body.birthday &&
+      // request.body.ss_id) {
       next();
   } else {
     response.status(400).json({
